b4042ca7fb1da1c5c100f5e40e51b56837f8ff8e
Raushan-Shahi <raushanshahi2001@gmail.com>
2024-02-28 19:41:47 +0530

    GIT API added

--- search.js
+++ search.js
@@ -12,7 +12,7 @@ function handleQuery(query) {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer API-KEY'   // we need to enter our API key here
+                'Authorization': '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'   // we need to enter our API key here
             },
             body: JSON.stringify({
                 prompt: query,
